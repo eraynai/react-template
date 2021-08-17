f961e5eb36d37f9e604473dd28194fae3ad5c0e1
Elli Raynai <elli@elliraynai.com>
2021-08-17 08:18:12 -0400

    set up structure of project

--- src/App.js
+++ src/App.js
@@ -1,7 +1,39 @@
 import './App.css';
+import React, { Component } from 'react';
+import { Switch, Route, Redirect } from 'react-router-dom';
 
-function App() {
-	return <div className='App'></div>;
+export default class App extends Component {
+	state = {
+		user: null,
+	};
+
+	setUserInState = (incomingUserData) => {
+		this.setState({ user: incomingUserData });
+	};
+
+	render() {
+		return (
+			<React.Fragment>
+				{this.state.user ? (
+					<Switch>
+						<Route
+							path='/index'
+							render={(props) => (
+								<DonationPage {...props} user={this.state.user} />
+							)}
+						/>
+						<Route
+							path='/profile'
+							render={(props) => (
+								<ProfilePage {...props} user={this.state.user} />
+							)}
+						/>
+						<Redirect to='/index' />
+					</Switch>
+				) : (
+					<AuthPage setUserInState={this.setUserInState} />
+				)}
+			</React.Fragment>
+		);
+	}
 }
-
-export default App;
